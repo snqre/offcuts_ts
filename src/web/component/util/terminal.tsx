8fdb201cd2fc98e52a1e2094a6206d0d8b506ec9
Marco Bizzaro <51394894+snqre@users.noreply.github.com>
2025-03-20 07:21:48 +0100

    *

--- src/web/component/util/terminal.tsx
+++ src/web/component/util/terminal.tsx
@@ -60,50 +60,50 @@ export function Terminal({last, next, onEval, style, ...more}: TerminalProps): R
                             {line}
                         </div>
                     </>)}
-                    <input
-                        style={{
-                            all: "unset",
-                            display: "flex",
-                            flexDirection: "row",
-                            justifyContent: "start",
-                            alignItems: "center",
-                            width: "100%",
-                            height: "auto",
-                            fontSize: "0.75em",
-                            fontWeight: "normal",
-                            fontFamily: Font[2]
-                        }}
-                        placeholder="Commands"
-                        type="text"
-                        value={next[0]}
-                        onChange={e => next[1](e.target.value)}
-                        onKeyDown={async e => {
-                            if (e.key !== "Enter") {
-                                return;
-                            }
-                            if (next[0].trim() === "") {
-                                return;
-                            }
-                            let input: string = next[0];
-                            let commands: Array<string> = input.split("|");
-                            next[1]("");
-                            last[1](last => [...last, [input]]);
-                            try {
-                                let response: Array<string> = (await onEval(commands));
-                                if (response.length === 0) {
-                                    last[1](last => [...last, ["Ok"]]);
-                                    return;
-                                }
-                                last[1](last => [...last, response]);
-                                return;
-                            }
-                            catch (e) {
-                                last[1](last => [...last, [String(e)]]);
-                                return;
-                            }
-                        }}/>
                 </div>
             </>)}
+            <input
+                style={{
+                    all: "unset",
+                    display: "flex",
+                    flexDirection: "row",
+                    justifyContent: "start",
+                    alignItems: "center",
+                    width: "100%",
+                    height: "auto",
+                    fontSize: "0.75em",
+                    fontWeight: "normal",
+                    fontFamily: Font[2]
+                }}
+                placeholder="Commands"
+                type="text"
+                value={next[0]}
+                onChange={e => next[1](e.target.value)}
+                onKeyDown={async e => {
+                    if (e.key !== "Enter") {
+                        return;
+                    }
+                    if (next[0].trim() === "") {
+                        return;
+                    }
+                    let input: string = next[0];
+                    let commands: Array<string> = input.split("|");
+                    next[1]("");
+                    last[1](last => [...last, [input]]);
+                    try {
+                        let response: Array<string> = (await onEval(commands));
+                        if (response.length === 0) {
+                            last[1](last => [...last, ["Ok"]]);
+                            return;
+                        }
+                        last[1](last => [...last, response]);
+                        return;
+                    }
+                    catch (e) {
+                        last[1](last => [...last, [String(e)]]);
+                        return;
+                    }
+                }}/>
         </div>
     </>;
 }
